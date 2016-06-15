95ff771111b54bfe7917b283703232de878fa78a
isqo <qouiqaismail@gmail.com>
2016-06-15 11:40:11 +0200

    update

--- js/main.js
+++ js/main.js
@@ -13,9 +13,9 @@
   var z_scale=1;
   var player = $('#player');
   $("#box-scale").attr('scale',objToStr({x:x_scale,y:y_scale,z:z_scale}));
-  $("[x-rotation]").attr('rotation',objToStr({x:90,y:0,z:0}));
-  $("[y-rotation]").attr('rotation',objToStr({x:0,y:90,z:0}));
-  $("[no-rotation]").attr('rotation',objToStr({x:0,y:0,z:90}));
+  // $("[x-rotation]").attr('rotation',objToStr({x:90,y:0,z:0}));
+  // $("[y-rotation]").attr('rotation',objToStr({x:0,y:90,z:0}));
+  // $("[no-rotation]").attr('rotation',objToStr({x:0,y:0,z:90}));
   player.attr('position', objToStr({x:0,y:cynlinderHeight/2 + 5,z:12}));
   var old_el = null;
   var old_z = 0;
@@ -42,6 +42,7 @@
   init: function () {
     var el = this.el;
     el.addEventListener('collide', function (event) {
+
       var first_step = el.hasAttribute('first-step');
       if((old_el!=null && old_el.getAttribute("id") != el.getAttribute("id")) || old_el==null)
       {
@@ -77,6 +78,7 @@ var lock = false;
 var playerEl = document.querySelector('[camera]');
 playerEl.addEventListener('collide', function (e) {
   var el = e.detail.body.el;
+
   if(el.parentNode.parentNode.getAttribute("id") == "the-front-wall-door")
   {
     if(!lock)
@@ -101,6 +103,27 @@ playerEl.addEventListener('collide', function (e) {
         }
       }, 50);
     }
+}
+else if(el.parentNode.classList.contains('stairs'))
+{
+  var first_step = el.hasAttribute('first-step');
+  if((old_el!=null && old_el.getAttribute("id") != el.getAttribute("id")) || old_el==null)
+  {
+    var x =player.attr('position').x;
+    var y =player.attr('position').y;
+    var z =player.attr('position').z;
+    var new_z=z - 0.2;
+    var new_y=y + 1;
+    if((z < old_z && y > old_y) || first_step){
+      player.attr('position', objToStr({x:x,y:new_y,z:new_z}));
+      old_el = el;
+    }
+    else {
+      old_el = null;
+    }
+    old_z = new_z ;
+    old_y = new_y ;
+  }
 }
   // console.log('Player has collided with body #' + e.detail.body.id);
 });
